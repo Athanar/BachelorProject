7999eb62551b296a59c3bc36962acec13c72fcf4
Athanar <wrv668@alumni.ku.dk>
2021-04-23 15:59:42 +0200

    Extract and generate tables

--- MetadataExtractor.py
+++ MetadataExtractor.py
@@ -1,14 +1,19 @@
 from sqlalchemy import *
 from sqlalchemy.engine import reflection
 import pandas as pd
+import numpy as np
+import re
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OrdinalEncoder
 
-
-""" engine = create_engine('postgresql://postgres:Viteco2020@localhost/fwsmetadata')
-meta = MetaData()
-user_table = Table('vmd_client_role', meta, schema='vmd',autoload=True, autoload_with=engine)
-insp = reflection.Inspector.from_engine(engine)
-schemas = insp.get_schema_names()
-tables = [insp.get_table_names(schema=item) for item in schemas] """
+def checkLength(type_string):
+    check = re.search(r'\d+', type_string)
+    if check is not None:
+        return float(re.search(r'\d+', type_string).group(0))
+    return 0.0
 
 class MetadataExtractor(object):
     def __init__(self, dialect, username, password, database, *args, **kwargs):
@@ -24,34 +29,49 @@ class MetadataExtractor(object):
         cleaned_tables = {k: v for k, v in tables.items() if len(tables[k]) > 0}
         return cleaned_tables
 
-    def get_column_list(self, tables):
+    def get_column_list(self, table_dict):
         column_list = []
-        for schema in tables:
-            for table in tables[schema]:
+        for schema in table_dict:
+            for table in table_dict[schema]:
                 primary_key = self.inspector\
                                 .get_pk_constraint(table, schema=schema)
-                if primary_key['name'] != None:
+                if primary_key['name'] is not None:
                     uniques = self.inspector\
                                 .get_unique_constraints(table, schema=schema)
-                    columns = self.inspector.get_columns(table, schema=schema)
-                    df = pd.DataFrame(columns)
-                    df['primekey'] = df['name'].isin(primary_key['constrained_columns'])
-                    if len(uniques) > 0:
-                        df['unique'] = df['name'].isin(uniques[0]['column_names'])
-                    else:
-                        df['unique'] = False
-                    foreign_keys = self.inspector\
-                                .get_foreign_keys(table, schema=schema)
-                    if len(foreign_keys) > 0:
-                        df['foreignkey'] = df['name'].isin([col['constrained_columns'][0] for col in foreign_keys])
-                    else:
-                        df['foreignkey'] = False
-                    column_list.append(df)
-        return pd.concat(column_list, ignore_index=True)
-    
-        
+                else:
+                    uniques = []
+                columns = self.inspector.get_columns(table, schema=schema)
+                df = pd.DataFrame(columns)
+                df['primekey'] = df['name'].isin(primary_key['constrained_columns'])
+                if len(uniques) > 0:
+                    df['unique'] = df['name'].isin(uniques[0]['column_names'])
+                else:
+                    df['unique'] = False
+                foreign_keys = self.inspector\
+                            .get_foreign_keys(table, schema=schema)
+                if len(foreign_keys) > 0:
+                    df['foreignkey'] = df['name'].isin([col['constrained_columns'][0] for col in foreign_keys])
+                else:
+                    df['foreignkey'] = False
+                df['length'] = df['type'].apply(lambda x: checkLength(str(x)))
+                df['type'] = df['type'].apply(lambda x: re.search(r'\w+', str(x)).group(0).lower())
+                column_list.append(df)
+        return pd.concat(column_list, ignore_index=True).drop(columns=['name'])
 
-extractor = MetadataExtractor('postgresql', 'postgres', 'Viteco2020', 'fwsmetadata')
+extractor = MetadataExtractor('postgresql', 'postgres', 'Viteco2020', 'testdb')
 tables = extractor.get_all_tables()
-columns = extractor.get_column_list(tables) 
-print(columns)
+column_df = extractor.get_column_list(tables)
+encoder_one = OneHotEncoder(sparse=False)
+encoder_ord = OrdinalEncoder()
+onehot = encoder_one.fit_transform(column_df.values)
+ordinal = encoder_ord.fit_transform(column_df.values)
+kmeans = KMeans(n_clusters=2, random_state=0).fit(onehot)
+kmeans_ord = KMeans(n_clusters=2, random_state=0).fit(ordinal)
+gauss_one = GaussianMixture(n_components=2, random_state=0).fit(onehot)
+gauss_ord = GaussianMixture(n_components=2, random_state=0).fit(ordinal)
+column_df['kmeansOnehot'] = kmeans.labels_
+column_df['kmeansOrd'] = kmeans_ord.labels_
+column_df['gaussOne'] = gauss_one.predict(onehot)
+column_df['gaussOrd'] = gauss_ord.predict(ordinal)
+column_df['sum_pred'] = column_df.iloc[:, -4:-1].sum(axis=1)
+print(column_df)
---
+++ MetadataGenerator.py
@@ -0,0 +1,33 @@
+from sqlalchemy import *
+from sqlalchemy.engine import reflection
+import pandas as pd
+import numpy as np
+import re
+
+
+
+engine = create_engine('postgresql://postgres:Viteco2020@localhost/testdb')
+engine2 = create_engine('postgresql://postgres:Viteco2020@localhost/targetdb')
+meta = MetaData()
+meta.reflect(bind=engine)
+
+meta2 = MetaData()
+meta2.reflect(bind=engine2)
+
+meta3 = MetaData()
+creatable_tables = []
+
+cols = [Column(col.name, col.type) for col in meta.tables['weather_conditions'].columns]
+new_table = Table(
+    'new_table', meta3,
+    Column('test', VARCHAR(length=100)),
+    *cols
+)
+for table in meta.tables:
+    if table not in meta2.tables:
+        for column in meta.tables[table].columns:
+            meta.tables[table].columns[column.key].server_default = None
+        creatable_tables.append(meta.tables[table])
+    else:
+        continue
+meta3.create_all(engine2)
